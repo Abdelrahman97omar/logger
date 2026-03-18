aeefe46f27c08c4a4c082c177d7c216ce326c8e2
abdelrahman <abdelrahman.omar@marses.systems>
2026-03-18 14:57:16 +0200

    Add requirments file with pdf v2

--- installScript.sh
+++ installScript.sh
@@ -2,6 +2,7 @@ sed -i "s/__USER__/$USER/g" /home/${USER}/logger/logger.service
 sed -i "s/__ROBOTID__/$ROBOT_ID/g" /home/${USER}/logger/logger.service
 echo 12345 | sudo -S cp /home/${USER}/logger/logger.service /etc/systemd/system
 sudo systemctl enable logger.service
+pip install -r requirements.txt
 mkdir -p /home/${USER}/.logs/
 cd /home/${USER}/logger
 cp * /home/${USER}/.logs/
