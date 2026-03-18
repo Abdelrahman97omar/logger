7d615148cc6200d0050040d002615cf6fcd4112e
abdelrahman <abdelrahman.omar@marses.systems>
2026-03-18 14:50:48 +0200

    Fix missing robot ID. As the problem was service doesn't have the env varibale so it doesn't pass it to its childern processes. So I added the robot id as a variable in the serivce file

--- installScript.sh
+++ installScript.sh
@@ -1,4 +1,5 @@
 sed -i "s/__USER__/$USER/g" /home/${USER}/logger/logger.service
+sed -i "s/__ROBOTID__/$ROBOT_ID/g" /home/${USER}/logger/logger.service
 echo 12345 | sudo -S cp /home/${USER}/logger/logger.service /etc/systemd/system
 sudo systemctl enable logger.service
 mkdir -p /home/${USER}/.logs/
