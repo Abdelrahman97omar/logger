27e96efc7641b2185e0039f7eb283cefc8e0133d
abdelrahman <abdelrahman.omar@marses.systems>
2026-08-17 15:19:13 +0300

    fix issue in run_log file path

--- logger_system/run-log.sh
+++ logger_system/run-log.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
-python3 /home/${USER}/.logs/creatreport.py;
+python3 /home/${USER}/.local/lib/python3.10/site-packages/logger_system/creatreport.py;
 sleep 30;
-python3 /home/${USER}/.logs/loger.py;
+python3 /home/${USER}/.local/lib/python3.10/site-packages/logger_system/loger.py;
